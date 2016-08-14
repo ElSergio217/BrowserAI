573b7ea7bfb0a3769139772926838b71c8ecb70c
ElSergio217 <sergioa.garcia217@gmail.com>
2016-08-14 19:30:01 -0400

    Open commands added

--- speech.js
+++ speech.js
@@ -1,8 +1,20 @@
 var r = document.getElementById('result');
+var msg = new SpeechSynthesisUtterance();
+var voices = window.speechSynthesis.getVoices();
+msg.voice = voices[10]; // Note: some voices don't support altering params
+msg.voiceURI = 'native';
+msg.volume = 1; // 0 to 1
+msg.rate = 1.5; // 0.1 to 10
+msg.pitch = 2; //0 to 2
+msg.text = '';
+msg.lang = 'en-US';
 
 function startConverting () {
-		
+	//msg.text="Hello. how can I assist you today."
+	//speechSynthesis.speak(msg);
+	
 	if('webkitSpeechRecognition' in window){
+	
 		var speechRecognizer = new webkitSpeechRecognition();
 		speechRecognizer.continuous = true;
 		speechRecognizer.interimResults = true;
@@ -17,15 +29,22 @@ function startConverting () {
 				var transcript = event.results[i][0].transcript;
 				transcript.replace("\n", "<br>");
 				if(event.results[i].isFinal){
-					finalTranscripts += transcript;
-				}
-				else{
-					interimTranscripts += transcript;
+					finalTranscripts = transcript;
 				}
 			}
-			r.innerHTML = finalTranscripts + '<span style="color:#999">' + interimTranscripts + '</span>';
+			r.innerHTML = finalTranscripts;
+			speechRecognizer.stop();
+
+			if(finalTranscripts.startsWith("open")){
+				var site = finalTranscripts.replace("open ", "");
+				window.open("http://" + site + ".com",'_blank');
+				msg.text="opening " + site;
+				speechSynthesis.speak(msg);
+				finalTranscripts="";
+			}
+			
 		};
-		
+				
 		speechRecognizer.onerror = function (event) {
 			
 		};
